6e6bf6e1378ca156451e6a9888ad85870b0fb6ad
coderking3 <2196592083@qq.com>
2025-11-23 02:53:59 +0800

    🔧 chore: use @king-3/play-tsxg for run playground

--- cli/constants.ts
+++
@@ -1,4 +0,0 @@
-// Reserved parameter names that Node.js will consume
-export const NODE_RESERVED_NAMES = ['watch', 'version'] as const
-
-export type NodeReservedName = (typeof NODE_RESERVED_NAMES)[number]
